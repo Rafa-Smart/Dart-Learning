92fd166ed52ee105522038cc6fcb13b00fe221dd
Rafa Khadafi <rafakhadafi29@gmail.com>
2025-07-17 20:06:05 +0700

    item-collections

---
+++ bin/4.item-collections.dart
@@ -0,0 +1,69 @@
+void main() {
+  // https://chatgpt.com/c/6878e78b-439c-8009-8525-94e6f2d4d8be
+  var includeItem = true;
+  var items = [0, if (includeItem) 1, 2, 3]; // [0, 1, 2, 3]
+
+  // jadi maksdunya itu, jika includeItem itu true maka akan return 1
+  // jika false maka akan return 2, 3
+  // nah 2, 3 ini itu apa, dia adalah elemen elemen pertama setelah operasi ifnya
+  // kalo 0 adalah elemen pertama sebelum si ifnya
+
+  // ini juga sama
+  var name = 'apple';
+  var items2 = [0, if (name == 'orange') 1 else 10, 2, 3]; // [0, 10, 2, 3]
+  // jadi jika name itu == orange, maka return 1, jika tidak
+  // maka akan return 10, dan dilanjut dengan elemen 2, 3
+
+  // lalu ada pattern matching di dart
+  Object data = 123;
+  var typeInfo = [
+    if (data case int i) 'Data is an integer: $i',
+    if (data case String s) 'Data is a string: $s',
+    if (data case bool b) 'Data is a boolean: $b',
+    if (data case double d) 'Data is a double: $d',
+  ]; // [Data is an integer: 123, Data is a double: 123]
+
+  // jadi gini, pada if yang pertama
+  // itu gini, jika data kita matchingkan dnegan int
+  // /berati jika data itu sama polanya dengan int, maka nilai data akan disimpan
+  // edalam variable i, lalu akan return string 'Data is an integer: $i'
+  // dan begitu seterusnya
+
+  // contoh 2
+  var word = 'hello';
+  var items3 = [
+    1,
+    if (word case String(length: var wordLength)) wordLength,
+    3,
+  ]; // [1, 5, 3]
+
+  // jadi gini, disini kita cek, jika word itu matching dengan objek instance dari String
+  // (yg artinya dia adlah objek string)
+  // yang otomatis juga punya property length,
+  // maka kita ambil lengthnya pake destruction si lengthnya, lalu disimpan di var wordLength
+  // lalu return wordLength nya yaitu 3
+  // ini penjelasannya
+  // https://chatgpt.com/c/6878ed9f-648c-8009-8a6e-874baf22b80c
+
+  // contoh lagi
+  var orderDetails = ['Apples', 12, ''];
+  var summary = [
+    'Product: ${orderDetails[0]}',
+    if (orderDetails case [_, int qty, _]) 'Quantity: $qty',
+    if (orderDetails case [_, _, ''])
+      'Delivery: Not Started'
+    else
+      'Delivery: In Progress',
+  ]; // [Product: Apples, Quantity: 12, Delivery: Not Started]
+
+  // jaid gini di pengecekan if pertama
+  // itu di cek, jika orderDetails itu matching dengan pola List [_, int qty, _]
+  // jadi elemen pertamanya itu diabaikan, dan 
+  // elemen keduanya itu harus int, jika benar int -> masukan ke variable qty
+  // elemen ketiga diabaikan
+
+  // karena kondisi ini true, maka return string 'Quantity: $qty'
+
+  // dan begitu selanjutnya
+
+}
