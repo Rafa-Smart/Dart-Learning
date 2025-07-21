48890bbce0e161bc670751c335b8d9cf0694737b
Rafa Khadafi <rafakhadafi29@gmail.com>
2025-07-21 17:44:39 +0700

    sekolah

--- sekolah/1.belajar-dart.dart
+++ sekolah/1.belajar-dart.dart
@@ -1,14 +1,21 @@
 import 'dart:io';
 
-void main(){
-    print('program menghitung luas segi panjang');
+void main() {
+  print('program menghitung luas segi panjang');
     stdout.write("maukan panjang persegi panjang ? ");
     double panjang = double.parse(stdin.readLineSync()!);
 
     stdout.write("maukan lebar persegi panjang ? ");
     double lebar = double.parse(stdin.readLineSync()!);
 
-    final luas = panjang * lebar;
-    print('luas data: $luas');
+
+
+  hitungLuas(panjang, lebar);
+
 
 }
+  void hitungLuas(double panjang, double lebar) {
+
+    final luas = panjang * lebar;
+    print('luas data: $luas');
+  }
